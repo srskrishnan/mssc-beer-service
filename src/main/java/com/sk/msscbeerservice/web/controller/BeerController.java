cce396a0f638b14bb42ad3c733ab26b000959578
SK Ramasamy <k.rishnansrs@gmail.com>
2021-06-22 17:16:45 -0400

    Adding Validation

--- src/main/java/com/sk/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/sk/msscbeerservice/web/controller/BeerController.java
@@ -3,8 +3,10 @@ package com.sk.msscbeerservice.web.controller;
 import com.sk.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
@@ -20,14 +22,14 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer( @RequestBody @Validated BeerDto beerDto){
 
         //todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody  @Validated BeerDto beerDto){
 
         //todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
